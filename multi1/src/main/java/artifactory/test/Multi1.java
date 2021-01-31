373d6bee5955a856dc7491735aac758c4aed17d6
samrjfrog <76995890+samrjfrog@users.noreply.github.com>
2021-01-31 15:29:41 +0200

    Update Multi1.java

--- multi1/src/main/java/artifactory/test/Multi1.java
+++ multi1/src/main/java/artifactory/test/Multi1.java
@@ -5,6 +5,6 @@ package artifactory.test;
  */
 public class Multi1 {
     public static void main(String[] args) {
-        System.out.println("Hellow Again!!!!!!!");
+        System.out.println("Hello Again!!!!!!!");
     }
 }
